cf9311c7c7350f798e80af78c5bc208707a85ff0
iiaatmajaii <aatmaja.bulakh@gmail.com>
2021-06-23 10:22:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qrgenerator/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.qrgenerator;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private Button generate, scan;
+    private EditText name;
+    private ImageView qr_code;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generate = (Button) findViewById(R.id.generate);
+        scan = (Button) findViewById(R.id.scan);
+        name = (EditText) findViewById(R.id.Name);
+        qr_code = (ImageView) findViewById(R.id.qr_code);
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name = name.getText().toString();
+                if (Name!= null && !Name.isEmpty()){
+                    try {
+                        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                        BitMatrix bitMatrix = multiFormatWriter.encode( Name, BarcodeFormat.QR_CODE,1000,1000 );
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        qr_code.setImageBitmap(bitmap);
+                    }
+                        catch (WriterException e) {
+                        e.printStackTrace();
+                        }
+
+                }
+            }
+        });
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setOrientationLocked(false);
+                intentIntegrator.setPrompt("Scanning");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setBarcodeImageEnabled(true);
+                intentIntegrator.initiateScan();
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null && result.getContents() != null){
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Scanned Results")
+                    .setMessage(result.getContents())
+                    .setPositiveButton("Copy", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ClipboardManager manager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                            ClipData data = ClipData.newPlainText("Results", result.getContents());
+                            manager.setPrimaryClip(data);
+                        }
+                    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).create().show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
